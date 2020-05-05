966f4916c854c96f9b87fc3edc8a12fb5d861119
nicolasgranata <nicolasegranata@gmail.com>
2020-05-05 19:38:08 -0300

    Adding Logging to ErrorController

--- src/TodoSample.WebApi/V1/Controllers/ErrorController.cs
+++ src/TodoSample.WebApi/V1/Controllers/ErrorController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace TodoSample.WebApi.V1.Controllers
 {
@@ -7,6 +9,14 @@ namespace TodoSample.WebApi.V1.Controllers
     public class ErrorController : ControllerBase
     {
         [Route("/error")]
-        public IActionResult Error() => Problem();
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var errorMessage = context.Error.Message;
+
+            Log.Logger.Error(errorMessage, context.Error);
+
+            return Problem(detail: errorMessage, title: "An Unhandled Exception Occurred.");
+        }
     }
 }
