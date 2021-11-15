64f0edc8f2de7e90ecb8cb2099c26c8038b033cc
Matias Otero <nicolasegranata@gmail.com>
2021-11-15 11:26:52 -0300

    Upgrade to NET 6

--- src/TodoSample.WebApi/Program.cs
+++ src/TodoSample.WebApi/Program.cs
@@ -1,20 +1,48 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using TodoSample.Infrastructure.Extensions;
+using TodoSample.Services.Extensions;
+using TodoSample.WebApi.Middlewares;
+using TodoSample.WebApi.Swagger;
 
-namespace TodoSample.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+
+builder.Services.AddVersioning();
+
+builder.Services.AddSwagger();
+
+builder.Services.AddLoggingToPipeline();
+
+builder.Services.AddEntityFramework();
+
+var app = builder.Build();
+
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseSwaggerWithVersioning(apiVersionDescriptionProvider);
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    endpoints.MapControllers();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.Run();
+
+
+
